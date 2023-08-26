f1b1bf6a83ff8ebbfb93853d3dd0e16cb141a6b3
TeodoraAlina <alina.teower@gmail.com>
2023-08-26 08:32:17 +0000

    Complete challenge with hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import Sidebar from './components/Sidebar'
 import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
 import Content from './components/Content';
+import ContentHooks from './components/ContentHooks';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       {/* <NavBarSimple /> */}
       <NavBarForm />
       <Sidebar />
-      <Content />
+      <ContentHooks />
 
     </div>
   );
